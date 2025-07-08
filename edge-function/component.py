c573c360862c6ebc203e314164f4b2d033c6b5ea
Clement Bouvet <clement@cbouvet.fr>
2025-07-08 08:21:39 +0200

    helpers

--- edge-function/component.py
+++ edge-function/component.py
@@ -1,9 +1,13 @@
 from edge_function import exports
-from edge_function.types import Ok
+from edge_function.types import (Ok, Err)
 from edge_function.imports.types import (
     IncomingRequest, ResponseOutparam,
     OutgoingResponse, Fields, OutgoingBody
 )
+
+from edge_function.imports.streams import StreamError_Closed
+
+import json
 
 index = '''
 <!DOCTYPE html>
@@ -60,8 +64,41 @@ index = '''
 </html>
 '''
 
+def get_headers(request: IncomingRequest) -> dict:
+    fields = request.headers()
+    headers = dict(fields.entries())
+    return headers
+
+def get_settings(headers: dict) -> dict:
+    # Extract and return settings from headers
+    settings = headers.get("x-edgee-component-settings", "{}")
+    try:
+        return json.loads(settings)
+    except json.JSONDecodeError:
+        print("Invalid JSON in x-edgee-component-settings")
+        return {}
+
+def get_body(request: IncomingRequest) -> str:
+    body = request.consume()
+    stream = body.stream()
+    output = bytearray()
+    while True:
+        try:
+            chunk = stream.read(1024)
+            if not chunk:
+                break
+            output += bytearray(chunk)
+        except Err as e:
+                if isinstance(e.value, StreamError_Closed):
+                    break
+    return output.decode("utf-8")
+
 class IncomingHandler(exports.IncomingHandler):
     def handle(self, request: IncomingRequest, response_out: ResponseOutparam):
+        incomingHeaders = get_headers(request)
+        settings = get_settings(incomingHeaders)
+        body = get_body(request)
+
         outgoingResponse = OutgoingResponse(Fields.from_list([]))
         outgoingResponse.set_status_code(200)
         outgoingBody = outgoingResponse.body()
